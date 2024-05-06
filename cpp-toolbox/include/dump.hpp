9774da250389c876527368c21c7c97d2fb16d70a
BohdanQQ <40754203+BohdanQQ@users.noreply.github.com>
2024-05-07 00:53:49 +0200

    cpp-toolbox: modify clang format

--- cpp-toolbox/include/dump.hpp
+++ cpp-toolbox/include/dump.hpp
@@ -9,12 +9,12 @@
 namespace bqq_fileutils {
 template <typename T, typename S>
 concept into_span_of = requires(T a) {
-  std::span<S>(a);
+    std::span<S>(a);
 };
 
 template <typename T>
 concept has_value_type = requires(T) {
-  std::declval<typename T::value_type>();
+    std::declval<typename T::value_type>();
 };
 
 template <typename T, std::size_t SIZE>
@@ -27,10 +27,10 @@ concept iterable_of_item_size =
      into_span_of<T, const std::int8_t> || into_span_of<T, const char>);
 
 template <iterable_of_item_size<1> T>
-void dump(const T &data, const std::string &filename) {
-  std::ofstream file(filename, std::ios::binary);
-  file.write(reinterpret_cast<const char *>(data.data()), data.size());
+void dump(const T& data, const std::string& filename) {
+    std::ofstream file(filename, std::ios::binary);
+    file.write(reinterpret_cast<const char*>(data.data()), data.size());
 }
-}  // namespace bqq_fileutils
+}   // namespace bqq_fileutils
 
-#endif  // BQQ_DUMP_HPP
+#endif   // BQQ_DUMP_HPP
--- cpp-toolbox/include/move.hpp
+++ cpp-toolbox/include/move.hpp
@@ -1,6 +1,7 @@
 
 #ifndef BQQ_MOVE_HPP
 #define BQQ_MOVE_HPP
+
 /*
  * A replacement for std::move that checks for various pitfalls
  *
@@ -12,33 +13,34 @@
 
 template <typename DeclTypeT, bool IsIdExpression, typename T>
 constexpr std::remove_reference_t<T>&& move_impl(T&& obj) noexcept {
-  static_assert(!std::is_const_v<std::remove_reference_t<T>>,
-                "Cannot move const object");
-  static_assert(!std::is_lvalue_reference_v<DeclTypeT>,
-                "Cannot move lvalue, consider copying");
-  static_assert(IsIdExpression,
-                "Don't write MOVE(obj.member), write MOVE(obj).member instead");
+    static_assert(! std::is_const_v<std::remove_reference_t<T>>,
+                  "Cannot move const object");
+    static_assert(! std::is_lvalue_reference_v<DeclTypeT>,
+                  "Cannot move lvalue, consider copying");
+    static_assert(
+        IsIdExpression,
+        "Don't write MOVE(obj.member), write MOVE(obj).member instead");
 
-  return static_cast<std::remove_reference_t<T>&&>(obj);
+    return static_cast<std::remove_reference_t<T>&&>(obj);
 }
 
 // std::isalpha and std::isdigit are not constexpr in C++20
 #define move_help_isdigit(c) ((c) >= '0' && (c) <= '9')
 #define move_help_isalpha(c) \
-  ((c) >= 'a' && (c) <= 'z' || (c) >= 'A' && (c) <= 'Z')
+    ((c) >= 'a' && (c) <= 'z' || (c) >= 'A' && (c) <= 'Z')
 
 constexpr bool is_id_expression_impl(const char* const expr) noexcept {
-  for (auto str = expr; *str; ++str) {
-    if (!move_help_isalpha(*str) && !move_help_isdigit(*str) && *str != '_' &&
-        *str != ':') {
-      return false;
+    for (auto str = expr; *str; ++str) {
+        if (! move_help_isalpha(*str) && ! move_help_isdigit(*str) &&
+            *str != '_' && *str != ':') {
+            return false;
+        }
     }
-  }
-  return true;
+    return true;
 }
 
 #define is_id_expression(...) is_id_expression_impl(#__VA_ARGS__)
 #define MOVE(...) \
-  move_impl<decltype(__VA_ARGS__), is_id_expression(__VA_ARGS__)>(__VA_ARGS__)
+    move_impl<decltype(__VA_ARGS__), is_id_expression(__VA_ARGS__)>(__VA_ARGS__)
 
-#endif  // BQQ_MOVE_HPP
+#endif   // BQQ_MOVE_HPP
