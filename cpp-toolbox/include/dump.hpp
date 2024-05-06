dbd77b75616fcf50fe2fb5624df706e80a3f214a
BohdanQQ <40754203+BohdanQQ@users.noreply.github.com>
2024-05-07 00:16:53 +0200

    cpp-toolbox: clang-format

--- cpp-toolbox/include/dump.hpp
+++ cpp-toolbox/include/dump.hpp
@@ -1,37 +1,36 @@
 #ifndef BQQ_DUMP_HPP
 #define BQQ_DUMP_HPP
 
-#include <fstream>
-#include <string>
-#include <span>
 #include <concepts>
+#include <fstream>
+#include <span>
+#include <string>
 
-namespace bqq_fileutils
-{
-  template <typename T, typename S>
-  concept into_span_of = requires(T a) {
-    std::span<S>(a);
-  };
+namespace bqq_fileutils {
+template <typename T, typename S>
+concept into_span_of = requires(T a) {
+  std::span<S>(a);
+};
 
-  template <typename T>
-  concept has_value_type = requires(T) {
-    std::declval<typename T::value_type>();
-  };
+template <typename T>
+concept has_value_type = requires(T) {
+  std::declval<typename T::value_type>();
+};
 
-  template <typename T, std::size_t SIZE>
-  concept iterable_of_item_size =
-      has_value_type<T> &&
-      std::copyable<typename T::value_type> &&
-      std::is_trivially_copyable_v<typename T::value_type> &&
-      sizeof(typename T::value_type) == SIZE &&
-      (into_span_of<T, const std::byte> || into_span_of<T, const std::uint8_t> ||
-       into_span_of<T, const std::int8_t> || into_span_of<T, const char>);
+template <typename T, std::size_t SIZE>
+concept iterable_of_item_size =
+    has_value_type<T> && std::copyable<typename T::value_type> &&
+        std::is_trivially_copyable_v<typename T::value_type> &&
+        sizeof(typename T::value_type) == SIZE
+    &&
+    (into_span_of<T, const std::byte> || into_span_of<T, const std::uint8_t> ||
+     into_span_of<T, const std::int8_t> || into_span_of<T, const char>);
 
-  template <iterable_of_item_size<1> T>
-  void dump(const T &data, const std::string &filename) {
-    std::ofstream file(filename, std::ios::binary);
-    file.write(reinterpret_cast<const char *>(data.data()), data.size());
-  }
+template <iterable_of_item_size<1> T>
+void dump(const T &data, const std::string &filename) {
+  std::ofstream file(filename, std::ios::binary);
+  file.write(reinterpret_cast<const char *>(data.data()), data.size());
 }
+}  // namespace bqq_fileutils
 
-#endif // BQQ_DUMP_HPP
+#endif  // BQQ_DUMP_HPP
--- cpp-toolbox/include/move.hpp
+++ cpp-toolbox/include/move.hpp
@@ -2,32 +2,35 @@
 #ifndef BQQ_MOVE_HPP
 #define BQQ_MOVE_HPP
 /*
-* A replacement for std::move that checks for various pitfalls
-* 
-* Recommended usage: replace std::move with MOVE
-*
-* Adopted from https://www.youtube.com/watch?v=hvnl6T2MnUk 
-* (Jonathan Müller's talk in CPPCon 2024)
-*/ 
+ * A replacement for std::move that checks for various pitfalls
+ *
+ * Recommended usage: replace std::move with MOVE
+ *
+ * Adopted from https://www.youtube.com/watch?v=hvnl6T2MnUk
+ * (Jonathan Müller's talk in CPPCon 2024)
+ */
 
 template <typename DeclTypeT, bool IsIdExpression, typename T>
 constexpr std::remove_reference_t<T>&& move_impl(T&& obj) noexcept {
-  
-  static_assert(!std::is_const_v<std::remove_reference_t<T>>, "Cannot move const object");
-  static_assert(!std::is_lvalue_reference_v<DeclTypeT>, "Cannot move lvalue, consider copying");
-  static_assert(IsIdExpression, "Don't write MOVE(obj.member), write MOVE(obj).member instead");
-  
+  static_assert(!std::is_const_v<std::remove_reference_t<T>>,
+                "Cannot move const object");
+  static_assert(!std::is_lvalue_reference_v<DeclTypeT>,
+                "Cannot move lvalue, consider copying");
+  static_assert(IsIdExpression,
+                "Don't write MOVE(obj.member), write MOVE(obj).member instead");
+
   return static_cast<std::remove_reference_t<T>&&>(obj);
 }
 
 // std::isalpha and std::isdigit are not constexpr in C++20
 #define move_help_isdigit(c) ((c) >= '0' && (c) <= '9')
-#define move_help_isalpha(c) ((c) >= 'a' && (c) <= 'z' || (c) >= 'A' && (c) <= 'Z')
-
+#define move_help_isalpha(c) \
+  ((c) >= 'a' && (c) <= 'z' || (c) >= 'A' && (c) <= 'Z')
 
 constexpr bool is_id_expression_impl(const char* const expr) noexcept {
   for (auto str = expr; *str; ++str) {
-    if (!move_help_isalpha(*str) && !move_help_isdigit(*str) && *str != '_' && *str != ':') {
+    if (!move_help_isalpha(*str) && !move_help_isdigit(*str) && *str != '_' &&
+        *str != ':') {
       return false;
     }
   }
@@ -35,6 +38,7 @@ constexpr bool is_id_expression_impl(const char* const expr) noexcept {
 }
 
 #define is_id_expression(...) is_id_expression_impl(#__VA_ARGS__)
-#define MOVE(...) move_impl<decltype(__VA_ARGS__), is_id_expression(__VA_ARGS__)>(__VA_ARGS__)
+#define MOVE(...) \
+  move_impl<decltype(__VA_ARGS__), is_id_expression(__VA_ARGS__)>(__VA_ARGS__)
 
-#endif // BQQ_MOVE_HPP
+#endif  // BQQ_MOVE_HPP
